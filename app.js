69dc124b5236e33786a1b7a62696fdb65d2e3ae6
arsh019 <arshiyan.s@reapmind.com>
2022-05-15 16:00:29 +0530

    template engines

--- app.js
+++ app.js
@@ -1,32 +1,24 @@
-// const http = require('http');
-// const routes = require('./routes');
-// const server = http.createServer(routes);
+const path = require('path');
+
 const express = require('express');
 const bodyParser = require('body-parser');
-const path = require('path');
-const routeDir = require('./util/path');
+
 const app = express();
 
 app.set('view engine', 'pug');
 app.set('views', 'views');
 
 const adminData = require('./routes/admin');
-const shopRoute = require('./routes/shop');
+const shopRoutes = require('./routes/shop');
 
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
+
 app.use('/admin', adminData.routes);
-app.use(shopRoute);
+app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).sendFile(path.join(routeDir, 'views', '404.html'));
-})
+    res.status(404).render('404', {pageTitle: 'Page Not Found'});
+});
 
-
-
-
-// const server = http.createServer(app);
-app.listen(3000);
-// server.listen(3000, function () {
-// }
-// );
+app.listen(5000);
